d6cb5cdf21ef35dfb6a37fc1e48862d3f0fc415f
jerry.wxm <jerry.wxm@alibaba-inc.com>
2018-04-10 10:17:14 +0800

    add get clientIp

--- src/main/java/com/wxm/blog/controller/HomeController.java
+++ src/main/java/com/wxm/blog/controller/HomeController.java
@@ -1,6 +1,14 @@
 package com.wxm.blog.controller;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
+
+import com.alibaba.fastjson.JSONObject;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +23,32 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(HttpServletRequest request) {
-        String clientIp = request.getRemoteHost();
-        return "clientIp=" + clientIp;
+
+        try {
+
+            JSONObject jsonObject = new JSONObject();
+
+            Map<String, String> headerMap = new HashMap<>();
+            Enumeration<String> headerNames = request.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String headerKey = headerNames.nextElement();
+                headerMap.put(headerKey, request.getHeader(headerKey));
+            }
+
+            jsonObject.put("headerMap", headerMap);
+
+            jsonObject.put("remoteAddr", request.getRemoteAddr());
+
+            jsonObject.put("remoteHost", request.getRemoteHost());
+
+            return jsonObject.toJSONString();
+
+        } catch (Exception e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw, true);
+            e.printStackTrace(pw);
+            return sw.getBuffer().toString();
+        }
+
     }
 }
